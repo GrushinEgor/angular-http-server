3e21b97f82aeb6f66453faae51cd137238ebf727
GrushinEgor <egorgrushin@gmail.com>
2016-08-11 13:24:44 +0300

    Add svg mime-type. Add 404 error for *.ng.* files

--- angular-http-server.js
+++ angular-http-server.js
@@ -7,25 +7,36 @@ var fs = require("fs");
 
 var server = http.createServer(function (req, res) {
     var possibleFilename = req.url.slice(1) || "dummy";
+    possibleFilename = possibleFilename.split('?')[0];
 
     fs.stat(possibleFilename, function (err, stats) {
 
         var fileBuffer;
+        var fileExtension = possibleFilename.split('.');
 
         if (!err && stats.isFile()) {
 
-            var fileExtension = possibleFilename.split('.');
             // we need last part of array in case of e.g. xxxx.min.css
             fileExtension = fileExtension[fileExtension.length - 1];
 
             console.log("Sending file: %s", possibleFilename);
             fileBuffer = fs.readFileSync(possibleFilename);
-            res.writeHead(200, { 'Content-Type': 'text/' + fileExtension });
+            var contentType = fileExtension == 'svg' 
+                        ? 'image/svg+xml'
+                        : 'text/' + fileExtension;
+            res.writeHead(200, { 'Content-Type': contentType});
         }
         else {
-            console.log("Route %s, replacing with index.html", possibleFilename);
-            fileBuffer = fs.readFileSync("index.html");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            var isNgTemplate = fileExtension[fileExtension.length - 2] == 'ng';
+            if(isNgTemplate){
+                console.log("Route %s is ng-template, so 404", possibleFilename);
+                fileBuffer = '404 Not Found';
+                res.writeHead(404,  { 'Content-Type': 'text/html' });
+            }else{
+                console.log("Route %s, replacing with index.html", possibleFilename);
+                fileBuffer = fs.readFileSync("index.html");
+                res.writeHead(200, { 'Content-Type': 'text/html' });    
+            }
         }
 
         res.write(fileBuffer);
